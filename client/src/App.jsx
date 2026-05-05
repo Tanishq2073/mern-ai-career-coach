c4089f0000ccae1866a223922803a80c9afa7b89
Tanishq Gupta <tanishqg02@gmail.com>
2026-05-05 17:25:13 +0530

    final fix app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,30 +1,25 @@
 import { Routes, Route } from "react-router-dom";
+
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
-import Navbar from "./Components/Navbar";
+import Navbar from "./components/Navbar";
 import Profile from "./pages/Profile";
 import ResumeAnalyzer from "./pages/ResumeAnalyzer";
 import InterviewQuestions from "./pages/InterviewQuestions";
 
-<Route path="/interview" element={<InterviewQuestions />} />
-
-<Route path="/profile" element={<Profile />} />
 function App() {
   return (
     <>
       <Navbar />
-      <div className="page">
-        <div className="container">
-          <Routes>
-            <Route path="/" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/resume" element={<ResumeAnalyzer />} />
-            <Route path="/interview" element={<InterviewQuestions />} />
-          </Routes>
-        </div>
-      </div>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/resume" element={<ResumeAnalyzer />} />
+        <Route path="/interview" element={<InterviewQuestions />} />
+      </Routes>
     </>
   );
 }
