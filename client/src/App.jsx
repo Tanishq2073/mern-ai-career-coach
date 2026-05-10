01702328c44d1de9295da8c152bc5eb347c91869
Tanishq Gupta <tanishqg02@gmail.com>
2026-05-10 15:44:45 +0530

    fixed app import paths

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-import Login from "./pages/login";
+import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
 import Profile from "./pages/Profile";
