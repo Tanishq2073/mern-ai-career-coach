ad2507fff7f59f371708d8dd50685f2d035d48cc
Tanishq Gupta <tanishqg02@gmail.com>
2026-05-10 15:58:25 +0530

    folder names changed

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
+import "../styles/register.css";
 
 function Register() {
   const [name, setName] = useState("");
